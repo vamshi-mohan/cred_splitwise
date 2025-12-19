107a81bce8c3146ada7b711bcb70cb654430c95d
vamshi-mohan <147185719+vamshi-mohan@users.noreply.github.com>
2025-12-19 14:16:45 +0530

    Update AddFriends.js

--- client/src/components/Dashboard/AddFriends.js
+++ client/src/components/Dashboard/AddFriends.js
@@ -3,8 +3,7 @@ import "../../styles/Dashboard.css";
 import FriendList from './FriendList'
 export const AddFriend = (props)=>{
     return(
-        <div className = "AddFriend">
-         
+        <div className = "AddFriend">  
          <div className = "content">
          <div className = "friendHeader" >
             <label htmlFor="">FRIENDS</label>
@@ -18,4 +17,5 @@ export const AddFriend = (props)=>{
 
         </div>
     )
+
 }
